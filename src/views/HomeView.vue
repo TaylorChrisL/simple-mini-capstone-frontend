f8f34206c91652864f97a8608102f98e19eff7ad
Chris Taylor <ctaylor534@gmail.com>
2022-07-26 10:00:02 -0700

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to da shop!",
+      message2: "Products",
+      products: [
+        { id: 1, name: "Item1", price: 3.99 },
+        { id: 2, name: "Item1", price: 6.99 },
+        { id: 1, name: "Item1", price: 2.0 },
+      ],
+    };
+  },
+  created: function () {},
+  methods: {
+    resetMessages: function () {
+      this.message2 = "We are here now";
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h2>{{ message2 }}</h2>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h5>{{ product.name }}</h5>
+      <h6>{{ product.price }}</h6>
+    </div>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: rgb(87, 7, 216);
+}
+</style>
